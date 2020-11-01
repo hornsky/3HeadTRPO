c60ebd1c123b94f246d486b619ba7425bc1c4b9e
Alexandr Hornsky <alexhornsky@yandex.ru>
2020-11-01 19:13:03 +0700

    fix test

--- test/test.c
+++ test/test.c
@@ -10,7 +10,17 @@
 
 #define testfilepath "./test/jurnal.dat"
 
-TEST_CASE(find2)
+CTEST(testfilepath, find2) //Поиск символа в строке
 {
-    REQUIRE(find2(testfilepath));    
+	struct vot {char surname[10]; int grade_book; int grade[3];} pp1;
+	pp1.surname = "Hornsky";
+	pp1.grade_book = 2342342;
+	pp1.grade[0] = "3";
+	pp1.grade[1] = "2";
+	pp1.grade[2] = "4";
+	pp1.grade[3] = "5";
+	
+	void real = find2(pp1);
+
+	ASSERT_EQUAL(real);
 }
