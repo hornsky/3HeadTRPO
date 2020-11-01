e0aefcd449c1045d7a98de01d63b0cf1f0e0710e
Alexandr Hornsky <alexhornsky@yandex.ru>
2020-11-01 19:20:27 +0700

    fix test

--- test/test.c
+++ test/test.c
@@ -11,12 +11,12 @@
 CTEST(pp1, find2) //Поиск символа в строке
 {
 	struct vot {char surname[10]; int grade_book; int grade[3];} pp1;
-	pp1.surname = "Hornsky";
+	pp1.surname[0] = "Hornsky";
 	pp1.grade_book = 2342342;
-	pp1.grade[0] = "3";
-	pp1.grade[1] = "2";
-	pp1.grade[2] = "4";
-	pp1.grade[3] = "5";
+	pp1.grade[0] = 3;
+	pp1.grade[1] = 2;
+	pp1.grade[2] = 4;
+	pp1.grade[3] = 5;
 	
 	int real = find2(pp1);
 	const int expected = 0;
